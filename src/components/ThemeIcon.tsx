e7edfdce3e0ae0dba89b62faf5ba16e59c5e6ed3
Vitaly Turovsky <vital2580@icloud.com>
2022-01-26 10:13:19 +0300

    fix: use @iconify instead of @mdi to fix crash in production bundle

--- src/components/ThemeIcon.tsx
+++ src/components/ThemeIcon.tsx
@@ -1,5 +1,6 @@
-import { mdiMoonWaningCrescent, mdiWhiteBalanceSunny } from '@mdi/js'
-import Icon from '@mdi/react'
+import mdiMoonWaningCrescent from '@iconify-icons/mdi/moon-waning-crescent'
+import mdiWhiteBalanceSunny from '@iconify-icons/mdi/white-balance-sunny'
+import { Icon } from '@iconify/react'
 import React from 'react'
 import tw from 'twin.macro'
 
@@ -8,9 +9,9 @@ interface ComponentProps extends React.ComponentProps<'svg'> {
     active: boolean
 }
 
-const ThemeIcon: React.FC<ComponentProps> = ({ icon, active, ...props }) => {
-    const path = icon === 'moon' ? mdiMoonWaningCrescent : mdiWhiteBalanceSunny
-    return <Icon path={path} size="48px" css={[!active && tw`opacity-60 cursor-pointer`]} {...(props as any)} />
+const ThemeIcon: React.FC<ComponentProps> = ({ icon: iconProp, active, ...props }) => {
+    const icon = iconProp === 'moon' ? mdiMoonWaningCrescent : mdiWhiteBalanceSunny
+    return <Icon icon={icon} css={[tw`w-[48px] h-[48px]`, !active && tw`opacity-50 cursor-pointer`]} {...(props as any)} />
 }
 
 export default ThemeIcon
